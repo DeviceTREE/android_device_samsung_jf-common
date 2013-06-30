e8cc63a4c23a231c06ec3dda578b3a9a3c828e32
liquid0624 <rjbirnesser@gmail.com>
2013-06-30 17:14:46 -0400

    This looks better i think...

--- jf-common.mk
+++ jf-common.mk
@@ -26,8 +26,8 @@ PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
