b6efac9132f5a38bf0b6b091057d6e31c7463169
Sam Mortimer <sam@mortimer.me.uk>
2013-08-17 23:30:48 -0700

    jf-common: include bluetooth_loader.te
    Change-Id: Ic6c08253c46d242814f74bc4024807e29faaef81

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -85,6 +85,7 @@ BOARD_SEPOLICY_UNION += \
 	file_contexts \
 	property_contexts \
 	te_macros \
+	bluetooth_loader.te \
 	bridge.te \
 	camera.te \
 	conn_init.te \
@@ -93,8 +94,8 @@ BOARD_SEPOLICY_UNION += \
 	domain.te \
 	drmserver.te \
 	file.te \
-	kickstart.te \
 	init.te \
+	kickstart.te \
 	mediaserver.te \
 	mpdecision.te \
 	netmgrd.te \
